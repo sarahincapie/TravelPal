5668559c2ff85a47e84616a12e87626d3a463ded
Matt King <mattdanking@gmail.com>
2016-02-25 16:37:45 -0500

    sign out; changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'expenses#index'
+
   resources :expenses
   resources :trips
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225045952) do
+ActiveRecord::Schema.define(version: 20160225162452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20160225045952) do
 
   add_index "trips", ["user_id"], name: "index_trips_on_user_id", using: :btree
 
+  create_table "types", force: :cascade do |t|
+    t.string   "category"
+    t.integer  "expense_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "types", ["expense_id"], name: "index_types_on_expense_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -69,4 +78,5 @@ ActiveRecord::Schema.define(version: 20160225045952) do
 
   add_foreign_key "expenses", "users"
   add_foreign_key "trips", "users"
+  add_foreign_key "types", "expenses"
 end
