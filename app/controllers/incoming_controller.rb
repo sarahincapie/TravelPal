bc54253a3bba3230c32d8464b7f36056936144f3
Robert Tassy <Rtass001@fiu.edu>
2016-02-28 19:06:30 -0500

    reverted back to text/plain

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -9,6 +9,6 @@ class IncomingController < ApplicationController
       end
       # r.Message "What up bruh."
     end
-    render 'send_message.xml.erb', :content_type => 'text/html'
+    render 'send_message.xml.erb', :content_type => 'text/plain'
   end
 end
