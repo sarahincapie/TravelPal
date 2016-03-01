6f68850f86d2a35d2e9012df75c8a0789a0ae07a
Robert Tassy <Rtass001@fiu.edu>
2016-02-29 21:26:38 -0500

    added wassup bruh message

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,14 +1,13 @@
 class IncomingController < ApplicationController
   def send_message
     body = params[:Body]
-    p body
     @twiml = Twilio::TwiML::Response.new do |r|
       if body == "Hi" || "Hello" || "Test"
           r.Message "Hi there! I'm your TravelPal"
       else
           r.Message "I'll get back to you on that."
       end
-      # r.Message "What up bruh."
+      r.Message "What up bruh."
     end
     render 'send_message.xml.erb', :content_type => 'text/xml'
   end
