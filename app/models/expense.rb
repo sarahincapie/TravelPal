dc9cd32ecb3142ff0074f8f211c3d2ce2dbcb388
sarahincapie <saral85@gmail.com>
2016-03-02 18:24:35 -0500

    Added a to json method to expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -11,4 +11,14 @@ class Expense < ActiveRecord::Base
   enum option: [:Food, :Accommodation, :Transport, :Entertainment_Attractions, :Nature_Environment,
    :Culture, :Nightlife, :Sports_Outdoor, :Shopping, :Business, :Health, :Miscellaneous ]
 
+def get_expense_by_day
+
+	 array = %w(cost date)
+	 as_json.inject({}) do | hash, key, value | 
+	 	if array.include? key 
+	 		hash[key] = value
+	 	end 
+	 	hash 
+	 end 
+
 end
