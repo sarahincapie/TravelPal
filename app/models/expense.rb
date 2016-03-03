9a5161666fb57e11493b84698cb7192a93c5add6
Matt King <mattdanking@gmail.com>
2016-03-02 20:08:16 -0500

    add daily_budget to trips

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -11,4 +11,14 @@ class Expense < ActiveRecord::Base
   enum option: [:Food, :Accommodation, :Transport, :Entertainment_Attractions, :Nature_Environment,
    :Culture, :Nightlife, :Sports_Outdoor, :Shopping, :Business, :Health, :Miscellaneous ]
 
+   class << self
+     def report type
+       if type == 'today'
+        today = Date.today.strftime("%m/%d/%Y")
+        self.select("date, SUM(cost)").where("date='?'", today)
+      elsif type == 'weekly'
+        self.select("date, SUM(cost)").where("date BETWEEN ? AND ?", today, today-7.days)
+      end
+     end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302232055) do
+ActiveRecord::Schema.define(version: 20160302232958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,8 +77,9 @@ ActiveRecord::Schema.define(version: 20160302232055) do
   create_table "trips", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "daily_budget"
   end
 
   add_index "trips", ["user_id"], name: "index_trips_on_user_id", using: :btree
