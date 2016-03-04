3efb004fa6d48b82f3061fea7eb26c8ad6d686e8
sarahincapie <saral85@gmail.com>
2016-03-03 19:30:31 -0500

    changes on js for d3, c3 and chartist, playing with sample data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,4 @@
-new Chartist.Line('.ct-chart', {
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  series: [
-    [2, 3, 2, 4, 5],
-    [0, 2.5, 3, 2, 3],
-    [1, 2, 2.5, 3.5, 4]
-  ]
-}, {
-  width: 500,
-  height: 300
-});
+//= require jquery
+//= require d3
+//= require c3
+//= require_tree 
---
+++ app/assets/javascripts/expenses.js
@@ -0,0 +1,20 @@
+$(function () {
+	var chart = c3.generate({
+	    bindto: '#chart',
+	    data: {
+	      columns: [
+	        ['data1', 30, 200, 100, 400, 150, 250],
+	        ['data2', 50, 20, 10, 40, 15, 25]
+	      ]
+	    }
+	});
+	var chart1 = c3.generate({
+	    bindto: '#chart1',
+	    data: {
+	      columns: [
+	        ['data1', 30, 200, 100, 400, 150, 250],
+	        ['data2', 50, 20, 10, 40, 15, 25]
+	      ]
+	    }
+	});
+});
