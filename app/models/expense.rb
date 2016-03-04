495a55aed5bce51da6bf618888acf74953ae4c8d
Matt King <mattdanking@gmail.com>
2016-03-04 00:09:44 -0500

    added scope to user; fixed spent/balance methods

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -13,7 +13,6 @@ class Expense < ActiveRecord::Base
 
 
 # def get_expense_by_day
-
 # 	 array = %w(cost date)
 # 	 as_json.inject({}) do | hash, key, value | 
 # 	 	if array.include? key 
@@ -21,41 +20,38 @@ class Expense < ActiveRecord::Base
 # 	 	end 
 # 	 	hash 
 # 	 end 
-
 # 	end 
 
+  # class << self
+  #   def spent(type)
+  #     today = DateTime.now
+  #     total = 0
+  #     if type == 'today'
+  #       # self.select("created_at AS date, SUM(cost)").where("created_at BETWEEN ? AND ?", today.at_beginning_of_day, today.at_end_of_day)
+  #       self.select(:created_at, :cost).where(trip_id: current_user.trips.last.id, created_at: (today).at_beginning_of_day..today.at_end_of_day).each { |e| total += e.cost}        
+  #     elsif type == 'week'
+  #       self.select(:created_at, :cost).where(trip_id: current_user.trips.last.id, created_at: (today - 7).at_beginning_of_day..today.at_end_of_day).each { |e| total += e.cost}
+  #       #self.select("created_at AS date, SUM(cost)").where("created_at BETWEEN ? AND ?", (today - 7).at_beginning_of_day, today.at_end_of_day)
+  #     else type == 'month'
+  #       self.select(:created_at, :cost).where(trip_id: current_user.trips.last.id, created_at: (today - 30).at_beginning_of_day..today.at_end_of_day).each { |e| total += e.cost}
+  #       # self.select("created_at AS date, SUM(cost)").where("created_at BETWEEN ? AND ?", (today - 30).at_beginning_of_day, today.at_end_of_day)
+  #     end
+  #     total
+  #   end
+  # end
 
 
-  class << self
-    def spent(type)
-      today = DateTime.now
-      total = 0
-      if type == 'today'
-        # self.select("created_at AS date, SUM(cost)").where("created_at BETWEEN ? AND ?", today.at_beginning_of_day, today.at_end_of_day)
-        self.select(:created_at, :cost).where(trip_id: current_user.trips.id, created_at: (today).at_beginning_of_day..today.at_end_of_day).each { |e| total += e.cost}        
-      elsif type == 'week'
-        self.select(:created_at, :cost).where(created_at: (today - 7).at_beginning_of_day..today.at_end_of_day).each { |e| total += e.cost}
-        #self.select("created_at AS date, SUM(cost)").where("created_at BETWEEN ? AND ?", (today - 7).at_beginning_of_day, today.at_end_of_day)
-      else type == 'month'
-        self.select(:created_at, :cost).where(trip_id: current_user.trips.id, created_at: (today - 30).at_beginning_of_day..today.at_end_of_day).each { |e| total += e.cost}
-        # self.select("created_at AS date, SUM(cost)").where("created_at BETWEEN ? AND ?", (today - 30).at_beginning_of_day, today.at_end_of_day)
-      end
-      total
-    end
-  end
-
-
-  class << self
-    def balance(type)
-      if type == 'today'
-        self.trips.daily_budget - spent("today")
-      elsif type == 'week'
-        (self.trips.daily_budget * 7) - spent("week")
-      else type == 'month'
-        (self.trips.daily_budget * 30) - spent("month")
-      end
-    end
-  end
+  # class << self
+  #   def balance(type)
+  #     if type == 'today'
+  #       self.current_user.trips.last.daily_budget - spent("today")
+  #     elsif type == 'week'
+  #       (self.trips.last.daily_budget * 7) - spent("week")
+  #     else type == 'month'
+  #       (self.trips.last.daily_budget * 30) - spent("month")
+  #     end
+  #   end
+  # end
 
 end
 
