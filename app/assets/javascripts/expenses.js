17d7a126bf21451535889a6a88d87c82d7356f6f
sarahincapie <saral85@gmail.com>
2016-03-04 10:29:21 -0500

    working responsive donut chart with d3 responsive

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -1,3 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['Lulu', 50],
+            ['Olaf', 50],
+        ],
+        type : 'donut'
+    },
+    donut: {
+        title: "Dogs love:",
+    }
+});
