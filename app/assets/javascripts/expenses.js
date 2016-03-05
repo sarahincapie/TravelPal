b1645d54da9ddcbffed1b8377064cf6dcedf33c8
sarahincapie <saral85@gmail.com>
2016-03-04 19:45:01 -0500

    Deleted c3 directory and made multiple charts appear at the same time with append function

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -1,3 +1,4 @@
+
 var chart = c3.generate({
     data: {
         columns: [
@@ -12,3 +13,31 @@ var chart = c3.generate({
         title: "I'm spending my money on",
     }
 });
+
+$("#expense_chart").append(chart.element);
+
+var chart2 = c3.generate({
+    data: {
+        columns: [
+            ['monday', 200],
+            ['tuesday', 50],
+            ['wednesday', 60],
+            ['thursday', 80],
+            ['friday', 70],
+            ['Saturday', 80],
+            ['Sunday', 140],
+        ],
+        type : 'bar'
+    },
+    bar: {
+        title: "My weekly expenses",
+    }
+});
+
+
+
+
+$("#week_chart").append(chart2.element);
+
+
+
