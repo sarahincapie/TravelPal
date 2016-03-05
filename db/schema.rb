3246836b9438aab8323293c79cf1669efa37078f
Matt King <mattdanking@gmail.com>
2016-03-05 18:34:39 -0500

    added user_id to friends(pics)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305013513) do
+ActiveRecord::Schema.define(version: 20160305233402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,10 @@ ActiveRecord::Schema.define(version: 20160305013513) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "friends", ["user_id"], name: "index_friends_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -82,6 +85,15 @@ ActiveRecord::Schema.define(version: 20160305013513) do
 
   add_index "trips", ["user_id"], name: "index_trips_on_user_id", using: :btree
 
+  create_table "types", force: :cascade do |t|
+    t.string   "category"
+    t.integer  "expense_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "types", ["expense_id"], name: "index_types_on_expense_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -104,5 +116,7 @@ ActiveRecord::Schema.define(version: 20160305013513) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "expenses", "trips"
+  add_foreign_key "friends", "users"
   add_foreign_key "trips", "users"
+  add_foreign_key "types", "expenses"
 end
