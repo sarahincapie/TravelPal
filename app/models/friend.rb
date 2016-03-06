cfc07b5d03e81b4dd2a4c4af8557b66d178bf3c5
Matt King <mattdanking@gmail.com>
2016-03-05 22:56:41 -0500

    fixed text photo upload

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -3,8 +3,9 @@ require "open-uri"
 class Friend < ActiveRecord::Base
 	  has_attached_file :avatar, styles: {
     thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
+    square: '200x200>',
+    medium: '300x300>',
+    large: '400X400>'
   }
 
   # Validate the attached image is image/jpg, image/png, etc
